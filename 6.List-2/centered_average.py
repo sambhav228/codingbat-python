596610cbb6c32bbcc9d5f3e0f36284a6827d8af7
Sambhav Kumar Thakur <sambhavkumar228@gmail.com>
2021-01-04 14:53:18 +0530

    Update centered_average.py

--- 6.List-2/centered_average.py
+++ 6.List-2/centered_average.py
@@ -7,11 +7,10 @@ and likewise for the largest value.
 Use int division to produce the final average.
 You may assume that the array is length 3 or more.
 
-Test Cases:
-
 centered_average([1, 2, 3, 4, 100]) → 3
 centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
 centered_average([-10, -4, -2, -4, -2, 0]) → -3
+centered_average([7, 7, 7]) → 7
 """
 
 
